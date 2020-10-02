820935edd5915312d9bda18cfd77aef4683af333
prsingh061000 <prsingh061000@gmail.com>
2020-10-02 17:38:05 +0530

    Contribution Done

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World."<<endl;
+  cout<<"Nobody can ask for pull requests..."<<endl;
+  //Add any fancy code you want! All pull requests are welcomed!
+  return 0;
+}
